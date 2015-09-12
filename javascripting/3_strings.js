1554698e82d56150d8e1e8268d0e9e9cd1ffa884
claudiopro <claudio.procida@gmail.com>
2015-09-12 18:34:25 +0200

    Added solutions to third and fourth exercises

---
+++ javascripting/3_strings.js
@@ -0,0 +1,4 @@
+// Define and assign a someString variable to the string value 'this is a string'
+var someString = 'this is a string';
+// Use console.log to print the variable someString to the terminal
+console.log(someString);
---
+++ javascripting/4_string_length.js
@@ -0,0 +1,6 @@
+// Create a variable named example
+var example;
+// Assign the string 'example string' to the variable example
+example = 'example string';
+// Use console.log to print the length of the string to the terminal.
+console.log(example.length);
