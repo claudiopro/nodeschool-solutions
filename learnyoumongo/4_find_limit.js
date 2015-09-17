f85503f4b1b9e82a8dea4bf9f8d3abf3e034e2b9
claudiopro <claudio.procida@gmail.com>
2015-09-17 21:53:47 +0200

    Refactored find solutions to use common util

--- learnyoumongo/4_find_limit.js
+++ learnyoumongo/4_find_limit.js
@@ -1,11 +1,12 @@
 /*
-Here we will learn how to search for documents.
-
-For all of the exercises, the database is learnyoumongo.
-So, the url would be something like: mongodb://localhost:27017/learnyoumongo
+Here we will learn how to search for documents but only fetch the fields
+we need.
 
 Use the parrots collection to find all documents where age
 is greater than the first argument passed to your script.
+
+The difference from the last lesson will be that we only want the
+name and age properties
 
 Using console.log, print the documents to stdout.
 
@@ -13,25 +14,18 @@ Using console.log, print the documents to stdout.
 
 ## HINTS
 
-To connect to the database, one can use something like this:
-
-    var mongo = require('mongodb').MongoClient
-    mongo.connect(url, function(err, db) {
-      // db gives access to the database
-    })
-
-To get a collection, one can use db.collection('<collection name>').
-
 To find a document or documents, one needs to call find() on the collection.
 
 Find is a little bit different than what we are used to seeing.
-
-Keep in mind, process.argv is an array of strings. To convert to an integer, you could use parseInt()
 
 Here is an example:
 
     collection.find({
       name: 'foo'
+    }, {
+      name: 1
+    , age: 1
+    , _id: 0
     }).toArray(function(err, documents) {
     
     })
@@ -39,7 +33,10 @@ Here is an example:
 If your program does not finish executing, you may have forgotten to
 close the db. That can be done by calling db.close() after you
 have finished.
-
 */
 
-require('./find_util')()
+require('./find_util')({
+	'name' : 1
+	, 'age' : 1
+	, '_id' : 0
+})
---
+++ learnyoumongo/find_util.js
@@ -0,0 +1,36 @@
+var mongo = require('mongodb').MongoClient
+
+module.exports = function(limit) {
+	// URL to the database for this exercise
+	var url = 'mongodb://localhost:27017/learnyoumongo'
+	// 
+	var minAge = parseInt(process.argv[2], 10)
+	mongo.connect(url, function(err, db) {
+		// db gives access to the database
+		if (err) {
+			console.log(err)
+			return
+		}
+		// collection returns a named collection
+		db.collection('parrots')
+			// find executes a query on the collection
+			.find({
+			}, limit)
+			// toArray converts the results to an array
+			.toArray(function(err, docs) {
+				// let's create an array to store results
+				var ret = []
+				// iterate over the array
+				docs.forEach(function(doc) {
+					if (doc.age > minAge) {
+						// if the age is greater than minAge, add this item to the results
+						ret.push(doc)
+					}
+				})
+				// print the results
+				console.log(ret)
+				// don't forget to close the db connection
+				db.close()
+			})
+	})
+}
